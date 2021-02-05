3f69d795529a0784a6e85624c53ae75a62abecd9
benj5378 <benj5378@outlook.com>
2021-02-05 15:07:00 +0300

    Move authentication to module

--- admin/home.php
+++ admin/home.php
@@ -1,38 +1,6 @@
 <?php
 
-    include_once("./../credentials.php");
-
-    try {
-        $pdo = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
-        // set the PDO error mode to exception
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-
-    require __DIR__ . '/../vendor/autoload.php';
-
-    $auth = new \Delight\Auth\Auth($pdo, dbTablePrefix: "hvb_admin_");
-
-    if(!$auth->isLoggedIn()) {
-        try {
-            $auth->login($_POST['email'], $_POST['password']);
-        
-            echo 'User is logged in';
-        }
-        catch (\Delight\Auth\InvalidEmailException $e) {
-            die('Wrong email address');
-        }
-        catch (\Delight\Auth\InvalidPasswordException $e) {
-            die('Wrong password');
-        }
-        catch (\Delight\Auth\EmailNotVerifiedException $e) {
-            die('Email not verified');
-        }
-        catch (\Delight\Auth\TooManyRequestsException $e) {
-            die('Too many requests');
-        }
-    }
+require_once "authenticate.php";
 
 ?>
 
